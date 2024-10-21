0015d336ba4a0b952b018661ea1260b115c056ff
Dimas Fahrul <dfpa.05@gmail.com>
2024-10-21 19:40:53 +0700

    add: firebase machine learning

---
+++ firebase-ml/FirebaseMachineLearning/app/src/main/java/com/example/firebasemachinelearning/PredictionHelper.kt
@@ -0,0 +1,99 @@
+package com.example.firebasemachinelearning
+
+import android.content.Context
+import android.util.Log
+import com.google.android.gms.tflite.client.TfLiteInitializationOptions
+import com.google.android.gms.tflite.gpu.support.TfLiteGpu
+import com.google.android.gms.tflite.java.TfLite
+import com.google.firebase.ml.modeldownloader.CustomModel
+import com.google.firebase.ml.modeldownloader.CustomModelDownloadConditions
+import com.google.firebase.ml.modeldownloader.DownloadType
+import com.google.firebase.ml.modeldownloader.FirebaseModelDownloader
+import org.tensorflow.lite.InterpreterApi
+import org.tensorflow.lite.gpu.GpuDelegateFactory
+import java.io.IOException
+import java.nio.ByteBuffer
+
+class PredictionHelper(
+    val context: Context,
+    private val onResult: (String) -> Unit,
+    private val onError: (String) -> Unit,
+    private val onDownloadSuccess: () -> Unit,
+) {
+    private var interpreter: InterpreterApi? = null
+
+    init {
+        TfLiteGpu.isGpuDelegateAvailable(context).onSuccessTask { gpuAvailable ->
+            val optionsBuilder = TfLiteInitializationOptions.builder()
+
+            if (gpuAvailable) optionsBuilder.setEnableGpuDelegateSupport(true)
+            TfLite.initialize(context, optionsBuilder.build())
+        }.addOnSuccessListener {
+            downloadModel()
+        }
+            .addOnFailureListener { onError(context.getString(R.string.tflite_is_not_initialized_yet)) }
+    }
+
+    @Synchronized
+    private fun downloadModel() {
+        val conditions = CustomModelDownloadConditions.Builder()
+            .requireWifi()
+            .build()
+
+        FirebaseModelDownloader.getInstance()
+            .getModel("Rice-Stock", DownloadType.LOCAL_MODEL, conditions)
+            .addOnSuccessListener { model: CustomModel ->
+                try {
+                    onDownloadSuccess()
+                    initializeInterpreter(model)
+                } catch (e: IOException) {
+                    onError(e.message.toString())
+                    Log.e(TAG, e.message.toString())
+                }
+            }
+            .addOnFailureListener {
+                onError(context.getString(R.string.firebaseml_model_download_failed))
+            }
+    }
+
+    fun predict(inputString: String) {
+        if (interpreter == null) {
+            return
+        }
+
+        val inputArray = FloatArray(1)
+        inputArray[0] = inputString.toFloat()
+        val outputArray = Array(1) { FloatArray(1) }
+        try {
+            interpreter?.run(inputArray, outputArray)
+            onResult(outputArray[0][0].toString())
+        } catch (e: Exception) {
+            onError(context.getString(R.string.no_tflite_interpreter_loaded))
+            Log.e(TAG, e.message.toString())
+        }
+    }
+
+    private fun initializeInterpreter(model: Any) {
+        interpreter?.close()
+
+        try {
+            val options = InterpreterApi.Options()
+                .setRuntime(InterpreterApi.Options.TfLiteRuntime.FROM_SYSTEM_ONLY)
+                .addDelegateFactory(GpuDelegateFactory())
+            if (model is ByteBuffer) {
+                interpreter = InterpreterApi.create(model, options)
+            } else if (model is CustomModel) {
+                model.file?.let {
+                    interpreter = InterpreterApi.create(it, options)
+                }
+            }
+        } catch (e: Exception) {
+            onError(e.message.toString())
+            Log.e(TAG, e.message.toString())
+        }
+    }
+
+    companion object {
+        private const val TAG = "PredictionHelper"
+    }
+}
